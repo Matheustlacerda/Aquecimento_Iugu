27957394daadc7af5a3730b5946aba72800f7333
Matheus_Lacerda <matheus_mtl@hotmail.com>
2021-03-04 16:13:34 -0300

    Código com problemas na hora da busca

--- study_diary.rb
+++ study_diary.rb
@@ -1,4 +1,11 @@
 require_relative 'study_item'
+require 'json'
+
+INSERT  = 1
+LIST    = 2
+SEARCH  = 3
+STUDIED = 4
+EXIT    = 5
 
 def clear
   system('clear')
@@ -10,7 +17,7 @@ def wait_keypress
   gets
 end
 
-def clear_and_wait_keypress
+def wait_keypress_and_clear
   wait_keypress
   clear
 end
@@ -19,68 +26,48 @@ def welcome
   'Bem-vindo ao Diário de Estudos, seu companheiro para estudar!'
 end
 
-INSERT = 1
-LIST = 2
-SEARCH = 3
-EXIT = 4
-
-
 def menu
   puts "[#{INSERT}] Cadastrar um item para estudar"
   puts "[#{LIST}] Ver todos os itens cadastrados"
   puts "[#{SEARCH}] Buscar um item de estudo"
+  puts "[#{STUDIED}] Informar item estudado"
   puts "[#{EXIT}] Sair"
   print 'Escolha uma opção: '
   gets.to_i
 end
 
-def register_study_item
-  print 'Digite o título do seu item de estudo: '
-  title = gets.chomp
-  print 'Digite a categoria do seu item de estudo: '
-  category = gets.chomp
-  puts "Item '#{title}' da categoria '#{category}' cadastrado com sucesso!"
-  StudyItem.new(title, category)
-end
-
-def print_items(collection)
-  collection.each_with_index do |item, index|
-    puts "##{index + 1} - #{item.title} - #{item.category}"
-  end
-  puts 'Nenhum item cadastrado' if collection.empty?
-end
-
-def search_items(collection)
+def search_items
   print 'Digite uma palavra para procurar: '
   term = gets.chomp
-  found_items = collection.filter do |item|
-    item.title.include?(term)
+  found_items = StudyItem.all.filter do |item|
+    item.include?(term) || item.category.include?(term)
   end
-  print_items(found_items)
-  puts 'Nenhum item encontrado' if collection.empty?
+  print found_items
 end
 
 clear
 puts welcome
-study_items = []
 option = menu
 
 loop do
   clear
   case option
-  when 1
-    study_items << register_study_item
-  when 2
-    print_items(study_items)
-  when 3
-    search_items(study_items)
-  when 4
+  when INSERT
+    StudyItem.register
+  when LIST
+    puts StudyItem.all
+    puts 'Nenhum item encontrado' if StudyItem.all.empty?
+  when SEARCH
+   StudyItem.search_items   
+  when STUDIED
+    StudyItem.studied_items
+  when EXIT
     clear
     puts 'Obrigado por usar o Diário de Estudos'
     break
   else
     puts 'Opção inválida'
   end
-  clear_and_wait_keypress
+  wait_keypress_and_clear
   option = menu
 end
--- study_item.rb
+++ study_item.rb
@@ -1,8 +1,56 @@
 class StudyItem
-    attr_reader :title, :category
-  
-    def initialize(title, category)
-      @title = title
-      @category = category
-    end
+  attr_reader :id, :title, :category
+
+  @@next_id = 1
+  @@study_items = []
+
+  def initialize(title:, category:)
+    @id = @@next_id
+    @title = title
+    @category = category
+
+    @@next_id += 1
+    @@study_items << self # adicionar uma gravação em arquivo em txt ou json, ver como fazer  
   end
+
+  def include?(query)
+    title.downcase.include?(query.downcase) || category.downcase.include?(query.downcase)
+  end
+
+  def to_s
+    "##{id} - #{title} - #{category}" 
+  end
+  
+  def self.register
+    print 'Digite o título do seu item de estudo: '
+    title = gets.chomp
+    print 'Digite a categoria do seu item de estudo: '
+    category = gets.chomp
+    puts "Item '#{title}' da categoria '#{category}' cadastrado com sucesso!"
+    new(title: title, category: category)
+  end
+
+  def self.all
+    @@study_items #fazer uma leitura de aquivo    
+  end
+
+  def self.search_items
+    print 'Digite uma palavra para procurar: '
+    term = gets.chomp
+    found_items = all.filter do |item|
+      item.include?(term)
+    end
+    puts found_items
+    puts 'Nenhum item encontrado' if all.empty?  #quando já tem um item não está mostrando essa informação
+  end    
+
+  def self.studied_items
+    puts '----------  Lista de itens  ----------'
+    puts @@study_items
+    puts
+    puts 'Qual o id do item já foi finalizado o estudo?'
+    id = gets.to_i
+    study_item = all.detect { |study_item| study_item.id == id }
+  end
+ 
+end
